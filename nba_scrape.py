4e15b579a93ff0d145611b73fec00d8ee463cf45
Brendan Fitzpatrick <brendanjfitzpatrick0@gmail.com>
2019-05-26 11:41:52 -0400

    Add four quad box plot to nba_scrape.py

--- nba_scrape.py
+++ nba_scrape.py
@@ -14,6 +14,7 @@ import requests
 from bs4 import BeautifulSoup
 from collections import defaultdict
 import seaborn as sns
+import matplotlib.pyplot as plt
 sns.set(style='whitegrid')
 
 url = 'http://www.espn.com/nba/seasonleaders'
@@ -84,10 +85,26 @@ for i, player in enumerate(team_soup_html.stripped_strings):
 df_all_nba = (pd.DataFrame(all_nba)
               .query('player != "Official release & voting totals"'))
 
+clay_mask = df.player == 'Klay Thompson'
 df = (pd.merge(df, df_all_nba, how='outer', on='player')
-      .assign(all_nba_team=lambda x: x.all_nba_team.fillna('N/A')))
+      .assign(all_nba_team=lambda x: (x.player.where(clay_mask, 
+                                                     (x.all_nba_team
+                                                       .fillna('N/A'))))))
+def nba_stat_bar_plot(ax, metric):
+    palette = ['#33C8FF', '#3396FF', '#3364FF', '#33FF44', '#FF3361']
+    hue_order = ['first', 'second', 'third', 'Klay Thompson', 'N/A']
+    (df.loc[df.position.isin(['SG', 'PG']), ['player', metric, 'all_nba_team']]
+     .nlargest(15, metric)
+     .pipe((sns.barplot, 'data'), x=metric, y='player', hue='all_nba_team',
+           hue_order=hue_order, orient='h', dodge=False,
+           palette=palette, ax=ax))
+    ax.set(xlabel=metric.upper(), ylabel='', 
+           title='NBA Guards 2018-2019 %s' % metric.upper())
 
-ax = (df.loc[df.position.isin(['SG', 'PG']), ['player', 'pts', 'all_nba_team']]
-      .nlargest(15, 'pts')
-      .pipe((sns.barplot, 'data'), x='pts', y='player', hue='all_nba_team'))
-ax.set(xlabel='Points Per Game', ylabel='', title='NBA Guards 2018-2019 PPG')
+f, axes = plt.subplots(2, 2, figsize=(25, 12.5))
+sns.despine()
+stats = ['pts', 'apg', 'rpg', 'stpg']
+for stat in stats:
+    df[stat] = df[stat].astype('float64')
+for i, ax in enumerate(axes.reshape(-1)):
+    nba_stat_bar_plot(ax, stats[i])
