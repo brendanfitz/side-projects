e5db58786ee467a8c4d3fa26f9fc009f4c8908ae
Brendan Fitzpatrick <brendanjfitzpatrick0@gmail.com>
2020-03-26 14:13:40 -0400

    Update yahoo_finance_api.py with full api functions

--- FinanceAndEconomics/yahoo_finance_api.py
+++ FinanceAndEconomics/yahoo_finance_api.py
@@ -10,31 +10,45 @@ import urllib.parse
 import datetime as dt
 import requests
 
-def calculate_epoch(date):
-    return (date - dt.datetime(1970,1,1)).total_seconds()
 
+def calculate_epoch(date):
+    return (date - dt.datetime(1970, 1, 1)).total_seconds()
+
+def create_url(ticker, start_date, end_date):
+    domain = "https://finance.yahoo.com/"
+    
+    ticker_url_encoded = urllib.parse.quote(ticker)
+    path = "quote/{}/history".format(ticker_url_encoded)
+    
+    epoch_start = calculate_epoch(start_date + dt.timedelta(days=1))
+    epoch_end = calculate_epoch(end_date + dt.timedelta(days=1))
+    qstr = ("?period1={:.0f}&period2={:.0f}&interval=1d&filter=history&frequency=1d"
+            .format(epoch_start, epoch_end))
+    
+    url = domain + path + qstr
+    return url
+
+
+def scrape_daily_stock_price_df(ticker, start_date, end_date):
+    url = create_url(ticker, start_date, end_date)
+
+    response = requests.get(url)
+    
+    df = (pd.read_html(response.text)[0].iloc[0:-1]
+        .rename(columns=lambda x: x.replace(' ', '_').replace('*', '').lower())
+        .assign(date=lambda x: pd.to_datetime(x.date))
+        .set_index('date')
+        .apply(lambda x: pd.to_numeric(x))
+    )
+    
+    return df
 
 """ future parameters """
 ticker = "^GSPC"
-start_date = dt.datetime(1927, 12, 29, 0, 0)
+start_date = dt.datetime(2020, 1, 1, 0, 0)
 end_date = dt.datetime(2020, 3, 23)
+df = scrape_daily_stock_price_df(ticker, start_date, end_date)
 
+df.assign()
 
-""" uri """
-ticker_url_encoded = urllib.parse.quote(ticker)
-uri = "https://finance.yahoo.com/quote/{}/history".format(ticker_url_encoded)
-
-epoch_start = calculate_epoch(start_date + dt.timedelta(days=1))
-epoch_end = calculate_epoch(end_date + dt.timedelta(days=1))
-qstr = ("period1={:.0f}&period2={:.0f}&interval=1d&filter=history&frequency=1d"
-        .format(epoch_start, epoch_end))
-url = uri + "?" + qstr
-
-response = requests.get(url)
-
-df = pd.read_html(response.text)[0].iloc[0:-1]
-
-df = (df.rename(columns=lambda x: x.replace(' ', '_').replace('*', '').lower())
-    .set_index('date')
-    .apply(lambda x: pd.to_numeric(x))
-)
+          
