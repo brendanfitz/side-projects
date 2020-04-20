bcf98ccb2f7a34285121be5dfdaa383de44d2a69
Brendan Fitzpatrick <brendanjfitzpatrick0@gmail.com>
2020-04-20 10:43:28 -0400

    Update boto3 test for metis project pickle download

--- Other/boto_test.py
+++ Other/boto_test.py
@@ -8,17 +8,41 @@ Created on Mon Apr 13 12:01:43 2020
 
 import os
 import boto3
-from aws_config import ACCESS_KEY, SECRET_KEY
+import os
+
+AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
+AWS_SECRET_KEY = os.getenv('AWS_SECRET_KEY')
+
 
 s3 = boto3.client(
     's3',
-    aws_access_key_id=ACCESS_KEY,
-    aws_secret_access_key=SECRET_KEY,
+    aws_access_key_id=AWS_ACCESS_KEY_ID,
+    aws_secret_access_key=AWS_SECRET_KEY,
 )
 
-
 bucket_name = 'metis-projects'
-object_name = '^GSPC.csv'
-file_name = os.path.join(os.path.expanduser('~'), 'Downloads', '^GSPC.csv')
+object_name = 'pickles/budget_poly_scaler.pkl'
+file_name = os.path.join(os.path.expanduser('~'), 'Downloads', 'budget_poly_scaler.pkl')
 
-s3.download_file(bucket_name, object_name, file_name)
+response = s3.download_file(bucket_name, object_name, file_name)
+
+def aws_download(object_name, filename=None,
+    bucket_name='metis-projects',
+    bucket_directory='pickles',
+    local_directory='metis_app/static/pickles'):
+
+    object_path = bucket_directory + '/' + object_name
+
+    if filename:
+        download_path =  local_directory + '/' + filename
+    else:
+        download_path =  local_directory + '/' + object_name
+        
+    s3 = boto3.client(
+        's3',
+        aws_access_key_id=AWS_ACCESS_KEY_ID,
+        aws_secret_access_key=AWS_SECRET_KEY,
+    )
+    s3.download_file(bucket_name, object_path, download_path)
+    
+aws_download('luther_model.pkl')
