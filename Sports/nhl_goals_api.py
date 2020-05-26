fc45fea205a172db314e9d0610df26506d458db8
Brendan Fitzpatrick <brendanjfitzpatrick0@gmail.com>
2020-05-26 16:29:46 -0400

    Update url format in nhl_goals_api

--- Sports/nhl_goals_api.py
+++ Sports/nhl_goals_api.py
@@ -12,7 +12,17 @@ import json
 import copy
 import os
 
-url = 'https://api.nhle.com/stats/rest/en/skater/summary?isAggregate=false&isGame=false&sort=%5B%7B%22property%22:%22points%22,%22direction%22:%22DESC%22%7D,%7B%22property%22:%22goals%22,%22direction%22:%22DESC%22%7D,%7B%22property%22:%22assists%22,%22direction%22:%22DESC%22%7D%5D&start=101&limit=100&factCayenneExp=gamesPlayed%3E=1&cayenneExp=gameTypeId=2%20and%20seasonId%3C=20192020%20and%20seasonId%3E=20192020'
+url = ('https://api.nhle.com/stats/rest/en/skater/summary'
+       '?isAggregate=false'
+       '&isGame=false'
+       '&sort=%5B%7B%22property%22:%22goals%22,%22direction%22:%22DESC%22%7D%5D'
+       '&start=101'
+       '&limit=100'
+       '&factCayenneExp=gamesPlayed%3E=1'
+       '&cayenneExp=gameTypeId=2%20and%20'
+           'seasonId%3C=20192020%20and%20seasonId%3E=20192020'
+      )
+
 
 response = requests.get(url)
 data = json.loads(response.text)
