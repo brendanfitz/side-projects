91f6f0409753d1cb39bbc08b4a1fb6b11c5e572c
Brendan Fitzpatrick <brendanjfitzpatrick0@gmail.com>
2020-06-03 14:34:43 -0400

    Add create_df_players and create_df_yearly_avgs to nhl_player_analysis

--- Sports/nhl_player_analysis.py
+++ Sports/nhl_player_analysis.py
@@ -6,6 +6,7 @@ Created on Mon Jun  1 11:56:22 2020
 """
 
 import os
+import json
 import pandas as pd
 
 filename = os.path.join('data', 'nhl_player_data_1917-2020.csv')
@@ -31,19 +32,49 @@ def pre_proc(input_df):
     )
     
     df = df.sort_values(['skaterFullName', 'season_number'])
+
     return df
 
 data = data.pipe(pre_proc)
 
-player_name = 'Jaromir Jagr'
-mask = data.skaterFullName == player_name
-data.loc[mask, 'playerId'].unique()
+def all_time_ranking(metric):
+    return (metric.sort_values(ascending=False)
+            .rank(method="dense", ascending=False)
+           )
 
-data = df
+def create_df_players(input_df):
+    df = input_df.copy()
+    df = (df
+       .groupby('skaterFullName')
+       .agg(
+           points=('points', 'sum'),
+           goals=('goals', 'sum'),
+           assists=('assists', 'sum'),
+           gamesPlayed=('gamesPlayed', 'sum'),
+           rookie_year=('year_season_start', 'min'),
+           retirement_year=('year_season_end', 'max'),
+           teams=('teamAbbrevs', lambda x: ', '.join(x.unique()))
+       )
+       .assign(
+           points_ranking=lambda x: all_time_ranking(x.points),
+           goals_ranking=lambda x: all_time_ranking(x.goals),
+           assists_ranking=lambda x: all_time_ranking(x.assists),
+           gamesPlayed_ranking=lambda x: all_time_ranking(x.gamesPlayed),
+           years_played=lambda x: x.retirement_year - x.rookie_year
+       )
+    )
+    return df
+
+df_players = create_df_players(data)
 
 player_name = 'Mark Messier'
 mask = data.skaterFullName == player_name
-dim_cols = [
+data.loc[mask, 'playerId'].unique()
+
+player_name = 'Mark Messier'
+mask = data.skaterFullName == player_name
+columns = [
+    'skaterFullName',
     'year_season_start',
     'teamAbbrevs',
     'positionCode',
@@ -66,13 +97,32 @@ dim_cols = [
 ]
 df = data.loc[mask, columns]
 
-df_yearly_avgs = (data
-    .groupby('year_season_start')
-    .agg(
-        average_goals=pd.NamedAgg(column='goals', aggfunc='mean'),
-        average_assists=pd.NamedAgg(column='assists', aggfunc='mean'),
-   )
+(df
+ .loc[:, ['skaterFullName', 'goals']]
+ .assign(
+     goals_lag1Y=lambda x: x.goals.shift(1),
+     goals_lag2Y=lambda x: x.goals.shift(2),
+     goals_lag3Y=lambda x: x.goals.shift(3),
+     goals_lag4Y=lambda x: x.goals.shift(4),
+     goals_lag5Y=lambda x: x.goals.shift(5)
+ )
+ .to_excel('mark_messier_goals_lagged.xlsx')
 )
+
+
+def create_df_yearly_avgs(input_df):
+    df = (input_df
+        .copy()
+        .groupby('year_season_start')
+        .agg(
+            league_goals_average=('goals', 'mean'),
+            league_assists_average=('assists', 'mean'),
+       )
+    )
+    
+    return df
+
+df_yearly_avgs = create_df_yearly_avgs(data)
 
 def calc_goals_and_assists_by_year(input_df, to_dict=False):
     df = input_df.copy()
@@ -100,10 +150,18 @@ def calc_games_by_team(input_df, to_dict=False):
 
 games_by_team = df.pipe(calc_games_by_team, True)
 
-def calc_all_time_rankings(input_df, to_dict=False):
-    pass
+def calc_career_data(df_players, player_name, to_dict=False):
+    player_data = df_players.loc[player_name, ]
+    
+    if to_dict:
+        player_data_dict = player_data.to_dict()
+        player_data = {'skaterFullName': player_name}
+        player_data.update(player_data_dict)
+        return player_data
+    
+    return player_data
 
-all_time_rankings = df.pipe(calc_all_time_rankings, True)
+career_data = df_players.pipe(calc_career_data, 'Mark Messier', True)
 
 def create_photo_url(player_name):
     mask = data.skaterFullName == player_name
@@ -115,21 +173,11 @@ def create_photo_url(player_name):
 
 photo_url = create_photo_url(player_name)
 
-
-import json
-
 player_data = {
-    'player_info': {
-            'player_name': player_name,
-            'teams': games_by_team.index.tolist(),
-            'years_played': df.shape[0],
-            'rookie_year': df.year_season_start.min(),
-            'retirement_year': df.year_season_start.max() + 1,
-            'photo_url': photo_url,
-        },
-    'all_time_rankings': all_time_rankings,
+    'career_data': career_data,
     'goals_and_assists_by_year': goals_and_assists_by_year,
     'games_by_team': games_by_team,
+    'photo_url': photo_url
 }
 
 json.dumps(player_data)
