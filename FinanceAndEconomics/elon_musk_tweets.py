199581123ec9c5546f3112d251f4afb557ff6856
Brendan Fitzpatrick <brendanjfitzpatrick0@gmail.com>
2020-06-12 15:34:14 -0400

    Create first draft of elon_musk_tweets.py

---
+++ FinanceAndEconomics/elon_musk_tweets.py
@@ -0,0 +1,77 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jun 11 17:03:16 2020
+
+@author: Brendan Non-Admin
+"""
+
+import os
+import pandas as pd
+import requests
+import base64
+import json
+from twitter_config import tc
+import datetime as dt
+
+_base_url = r'https://api.twitter.com/'
+
+def authenticate():
+    key_secret = '{}:{}'.format(tc['api_key'], tc['api_secret_key']).encode('ascii')
+    b64_encoded_key = base64.b64encode(key_secret)
+    b64_encoded_key = b64_encoded_key.decode('ascii')
+    auth_url = '{}oauth2/token'.format(_base_url)
+    auth_headers = {
+        'Authorization': 'Basic {}'.format(b64_encoded_key),
+        'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8'
+    }
+    auth_data = {
+        'grant_type': 'client_credentials'
+    }
+    auth_resp = requests.post(auth_url, headers=auth_headers, data=auth_data)
+    access_token = auth_resp.json()['access_token']
+    return access_token
+
+
+def scrape_elon_musk_tweets(access_token, max_id=None, tesla_tweets_only=False):
+    request_headers = {
+        'Authorization': 'Bearer {}'.format(access_token)    
+    }
+    request_params = {
+        'screen_name': 'elonmusk',
+        'count': 200,
+    }
+    if max_id:
+        request_params['max_id'] = max_id
+    
+    url = '{}1.1/statuses/user_timeline.json'.format(_base_url)
+    response = requests.get(url, headers=request_headers, params=request_params)
+    
+    json_data = response.json()
+    df = pd.DataFrame(json_data)
+    df
+    
+    try:
+        next_max_id = df.id.min() - 1
+    except Exception as e:
+        print(request_params)
+        if 'errors' in df.columns.tolist():
+            print(df.errors)
+        raise e
+        
+    
+    if tesla_tweets_only:
+        mask = df.text.str.contains('Tesla')
+        df = df.loc[mask, :]
+    
+    return df, next_max_id
+
+frames = list()
+next_max_id = None
+access_token = authenticate()
+while True:
+    df, next_max_id = scrape_elon_musk_tweets(access_token, max_id=next_max_id)
+    frames.append(df)
+    if next_max_id is None:
+        break
+    
+df = pd.concat(frames)
--- Other/rangers_email.py
+++ Other/rangers_email.py
@@ -103,16 +103,12 @@ def main(argv):
     text = text_template.render(**template_kwargs)
     html = html_template.render(**template_kwargs)
 
-    # Turn these into plain/html MIMEText objects
     part1 = MIMEText(text, "plain")
     part2 = MIMEText(html, "html")
-
-    # Add HTML/plain-text parts to MIMEMultipart message
-    # The email client will try to render the last part first
     message.attach(part1)
     message.attach(part2)
 
-    # Create secure connection with server and send email
+    # Create secure connection with server
     context = ssl.create_default_context()
 
     with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
